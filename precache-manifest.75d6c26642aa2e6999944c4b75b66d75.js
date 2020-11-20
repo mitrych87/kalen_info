f97ccafba554b670ec0239c5636b5b29c784896e
mitrych87 <mitrych87@gmail.com>
2020-11-20 18:57:52 +0000

    Updates

---
+++ precache-manifest.75d6c26642aa2e6999944c4b75b66d75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "560da293cb0028bb8121",
+    "url": "/kalen_info/static/css/main.fa7ae332.chunk.css"
+  },
+  {
+    "revision": "560da293cb0028bb8121",
+    "url": "/kalen_info/static/js/main.eaafb770.chunk.js"
+  },
+  {
+    "revision": "df4e82da179da3b759c7",
+    "url": "/kalen_info/static/js/runtime~main.70c7cd53.js"
+  },
+  {
+    "revision": "97b6ae2954aa348fdb77",
+    "url": "/kalen_info/static/css/2.5a3c2c00.chunk.css"
+  },
+  {
+    "revision": "97b6ae2954aa348fdb77",
+    "url": "/kalen_info/static/js/2.395c951a.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/kalen_info/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "a168136697f9a3e8b03e9d073609b7c0",
+    "url": "/kalen_info/index.html"
+  }
+];
